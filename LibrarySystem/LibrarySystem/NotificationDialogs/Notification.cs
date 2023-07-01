a6ac067e0b61c6f25359b40e364211e6e329e885
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-07-01 18:16:52 +0200

    feat: Created static notification class
    There are successfull, error and warning dialogs

---
+++ LibrarySystem/LibrarySystem/NotificationDialogs/Notification.cs
@@ -0,0 +1,25 @@
+﻿using ZdravoCorp.MainUI.NotificationDialogs.Presentation;
+
+namespace ZdravoCorp.MainUI.NotificationDialogs
+{
+    public static class Notification
+    {
+        public static void ShowErrorDialog(string message)
+        {
+            ErrorDialog errorDialog = new(message);
+            errorDialog.ShowDialog();
+        }
+
+        public static void ShowSuccessDialog(string message)
+        {
+            SuccessDialog successDialog = new(message);
+            successDialog.ShowDialog();
+        }
+
+        public static void ShowWarningDialog(string message)
+        {
+            WarningDialog warningDialog = new(message);
+            warningDialog.ShowDialog();
+        }
+    }
+}
