ec3c3821efb54ba53ef1926cfa148a417bac4ab2
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-07-01 16:38:26 +0200

    feat: login window xaml

--- LibrarySystem/LibrarySystem/MainWindow.xaml.cs
+++ LibrarySystem/LibrarySystem/MainWindow.xaml.cs
@@ -24,5 +24,71 @@ namespace LibrarySystem
         {
             InitializeComponent();
         }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //User user = UserService.GetLoginUser(this.usernameTextbox.Text, this.passwordTextbox.Password);
+            //if (user == null)
+            //{
+            //    errorLabel.Visibility = Visibility.Visible;
+            //    return;
+            //}
+
+            //Globals.LoggedUser = user;
+            //try
+            //{
+            //    openRoleWindow(user);
+            //}
+            //catch (Exception error)
+            //{
+            //    Notification.ShowErrorDialog(error.Message);
+
+            //    return;
+            //}
+            //this.Close();
+        }
+
+        //private void openRoleWindow(User user)
+        //{
+        //    switch (user.Role)
+        //    {
+        //        //case User.UserRole.Doctor:
+        //        //    DoctorWindow dw = new DoctorWindow();
+        //        //    dw.Show();
+        //        //    break;
+        //        //case User.UserRole.Nurse:
+        //        //    NurseWindow nw = new NurseWindow();
+        //        //    nw.Show();
+        //        //    break;
+        //        //case User.UserRole.Patient:
+        //        //    if (PatientService.IsPatientBlocked(user.Username))
+        //        //    {
+        //        //        throw new InvalidOperationException("This account has been permanently disabled.");
+        //        //    }
+        //        //    else
+        //        //    {
+        //        //        PatientWindow pd = new PatientWindow();
+        //        //        pd.Show();
+        //        //    }
+        //        //    break;
+        //        //case User.UserRole.Manager:
+        //        //    ManagerWindow wm = new ManagerWindow();
+        //        //    wm.Show();
+        //        //    break;
+        //    }
+        //}
+
+        private void LoginWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //JobManager.Initialize(new InventoryTimerRegistry());
+            //RenovationSchedule.CheckRenovations();
+        }
+
+
+        private void loginWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                loginBtn_Click(sender, e);
+        }
     }
 }
