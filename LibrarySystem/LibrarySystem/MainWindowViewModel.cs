4691504d1d144dfa71d4070453ab69dc29f66df0
OgnjenGligoric <gligoric383@gmail.com>
2023-07-02 13:16:29 +0200

    feat: mvvm login

--- LibrarySystem/LibrarySystem/MainWindowViewModel.cs
+++ LibrarySystem/LibrarySystem/MainWindowViewModel.cs
@@ -8,14 +8,16 @@ using System.Threading.Tasks;
 using static LibrarySystem.Users.Account;
 using System.Windows.Input;
 using System.Windows;
+using LibrarySystem.MainCommands;
 using LibrarySystem.Utils;
 using ZdravoCorp.MainUI.NotificationDialogs;
+using ZdravoCorp.MainUI.NotificationDialogs.Presentation;
 
 namespace LibrarySystem
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private readonly AccountService _accountService;
+        
         private readonly MainWindow _mainWindow;
 
         private string? _username;
@@ -36,7 +38,7 @@ namespace LibrarySystem
         {
             get
             {
-                return _logIn ??= new 
+                return _logIn ??= new LogInCommand(this, _mainWindow);
             }
         }
 
@@ -53,59 +55,7 @@ namespace LibrarySystem
 
         public MainWindowViewModel(MainWindow mainWindow)
         {
-            _accountService = new(new AccountRepository());
             _mainWindow = mainWindow;
-        }
-
-        private void loginBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Account account = _accountService.GetAccount(this.usernameTextbox.Text, this.passwordTextbox.Password);
-            if (account == null)
-            {
-                errorLabel.Visibility = Visibility.Visible;
-                return;
-            }
-
-            //Globals.LoggedUser = user;
-            try
-            {
-                openRoleWindow(account);
-            }
-            catch (Exception error)
-            {
-                Notification.ShowErrorDialog(error.Message);
-                return;
-            }
-            this.Close();
-        }
-
-        private void openRoleWindow(Account account)
-        {
-            switch (account.Type)
-            {
-                case UserType.Administrator:
-                    AdminWindow adminWindow = new AdminWindow();
-                    adminWindow.Show();
-                    break;
-                case UserType.Librarian:
-                    LibrarianWindow librarianWindow = new LibrarianWindow();
-                    librarianWindow.Show();
-                    break;
-                case UserType.SpecializedLibrarian:
-                    SpecializedLibrarianWindow specializedLibrarianWindow = new SpecializedLibrarianWindow();
-                    specializedLibrarianWindow.Show();
-                    break;
-                case UserType.Member:
-                    MemberWindow memberWindow = new MemberWindow();
-                    memberWindow.Show();
-                    break;
-            }
-        }
-
-        private void loginWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-                loginBtn_Click(sender, e);
         }
     }
 }
