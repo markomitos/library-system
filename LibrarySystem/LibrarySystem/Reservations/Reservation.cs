e19c5cf3c7b1447bed50aa46a39f8a36ad2192f7
OgnjenGligoric <gligoric383@gmail.com>
2023-07-02 14:16:41 +0200

    feat: reservation domain

--- LibrarySystem/LibrarySystem/Reservations/Reservation.cs
+++ LibrarySystem/LibrarySystem/Reservations/Reservation.cs
@@ -8,5 +8,10 @@ namespace LibrarySystem.Reservations
 {
     public class Reservation
     {
+        public int Id { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string UserJMBG { get; set; }
+        public int? CopyId { get; set; }
+        public int TitleUDK { get; set; }
     }
 }
