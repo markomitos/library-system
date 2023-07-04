74ae208d9a4d891702d2fe74fe73d1f2b6268e9b
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-07-04 12:23:56 +0200

    feat: reorganized window layout and implemented mvvm

--- LibrarySystem/LibrarySystem/MainUI/LibrarianWindow.xaml.cs
+++ LibrarySystem/LibrarySystem/MainUI/LibrarianWindow.xaml.cs
@@ -15,6 +15,7 @@ using LibrarySystem.BookBorrowings.BookReturn;
 using LibrarySystem.Inventory.Books;
 using LibrarySystem.Inventory.Copies;
 using LibrarySystem.Inventory.Titles;
+using LibrarySystem.MainUI.ViewModels;
 using LibrarySystem.Users.GUI.Views;
 
 namespace LibrarySystem.MainUI
@@ -27,24 +28,7 @@ namespace LibrarySystem.MainUI
         public LibrarianWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            BookBorrowingView bbv = new BookBorrowingView();
-            bbv.ShowDialog();
-        }
-
-        private void handleMembersButton_Click(object sender, RoutedEventArgs e)
-        {
-            MembersHandlingWindow membersHandlingWindow = new MembersHandlingWindow();
-            membersHandlingWindow.ShowDialog();
-        }
-
-        private void ReturnBookButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            BookReturnWindow bookReturnWindow = new BookReturnWindow();
-            bookReturnWindow.ShowDialog();
+            DataContext = new LibrarianViewModel(this);
         }
     }
 }
